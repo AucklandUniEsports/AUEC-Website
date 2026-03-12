0d30ac2ef294a4d6c8855564872bf730f14800cc
BlackPri0r <lawrenceli630@gmail.com>
2026-03-12 20:40:05 +1300

    CHORE: added prettier + restructured components folder

--- app/components/SocialMediaLink.tsx
+++ app/components/SocialMediaLink.tsx
@@ -1,10 +1,10 @@
 type SocialMediaLinkProps = {
     icon: string;
     link: string;
-}
+};
 
-export default function SocialMediaLink({icon, link} : SocialMediaLinkProps){
-    return(
+export default function SocialMediaLink({ icon, link }: SocialMediaLinkProps) {
+    return (
         <a className="social-media-link" href={link} target="_blank">
             <img className="social-media-icon" src={icon} alt="" />
         </a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,92 +1,132 @@
-import EventCard from "./components/EventCard";
+import EventCard from "./components/events/EventCard";
 import StandardButton from "./components/StandardButton";
 import prisma from "@/lib/prisma";
 import SponsorCard from "./components/SponsorCard";
 
 const sponsors = [
-  {
-    link:"https://www.facebook.com/StandingFierce/",
-    img:"standing-fierce.svg"
-  },
-  {
-    link:"https://zowie.benq.com/en-au/index.html",
-    img:"zowie.svg"
-  },
-  {
-    link:"https://gameon.co.nz/",
-    img:"game-on.svg"
-  }
-
-]
+    {
+        link: "https://www.facebook.com/StandingFierce/",
+        img: "standing-fierce.svg",
+    },
+    {
+        link: "https://zowie.benq.com/en-au/index.html",
+        img: "zowie.svg",
+    },
+    {
+        link: "https://gameon.co.nz/",
+        img: "game-on.svg",
+    },
+];
 
 export default async function Home() {
-  const events = await prisma.event.findMany({
-    include: {
-      CategoriesOnEvents: {
+    const events = await prisma.event.findMany({
         include: {
-          Category: true,
+            CategoriesOnEvents: {
+                include: {
+                    Category: true,
+                },
+            },
+            Location: true,
         },
-      },
-      Location: true,
-    },
-    take: -3,
-  });
-  return (
-    <>
-      <section className="home-a atf-section">
-        <div className="home-a-content">
-          <h1 className="home-title">For The Love <br /> of The Game.</h1>
-          <p className="standard-text">Auckland University Esports Club (AUEC) is New Zealand's biggest student e-sports organization, empowering players to compete and enjoy gaming with their peers.</p>
-        </div>
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLScOvsq5IQ-yDr0wx3xYN3ZNJkCz1mP4Kw8NZl6XQ_HSc8rswg/viewform?usp=header"  target="_blank" className="button-standard button-lime">
-            Sign up to AUEC.
-        </a>
-      </section>
-      <section className="home-b">
-        <div className="home-b-top">
-          <h2 className="section-title">Featured Events</h2>
-          <StandardButton  title="Explore Events." color="grey" link="/events"/>
-        </div>
-        <div className="events-wrapper">
-          {
-            events.map((event, index) =>(
-              <EventCard event={event} key={index}/>
-            ))
-          }
-        </div>
-      </section>
-      <div className="divider"></div>
-      <section className="home-c">
-          <div className="home-c-img-wrapper-outer">
-            <div className="home-c-img-wrapper-left"><img className="home-c-img-left" src="DSC00032.webp" alt="" /></div>
-            <div className="home-c-img-wrapper-right">
-              <img className="home-c-img-right" src="DSC07430.webp" alt="" />
-              <img className="home-c-img-right" src="DSC00276.webp" alt="" />
-            </div>
-          </div>
-          <h2 className="home-title">Meet the <br /> Esports Club.</h2>
-          <p className="standard-text">
-            From fighting game locals to interclub tournaments, our team runs a bunch of events for the community and puts a lot of work into our broadcasts for the folks at home. Whether you want to grind matches or get involved behind the scenes, AUEC has a place for you.
-          </p>
-          <StandardButton  title="Learn More." color="grey" link="/about"/>
-      </section>
-      <div className="divider"></div>
-      <section className="home-b">
-        <div className="home-b-top">
-          <h2 className="section-title">Community Partners</h2>
-          <a href="AUEC 2026 Sponsorship Deck.pdf" target="_blank" className="button-standard button-grey">
-            About Sponsorships.
-          </a>
-        </div>
-        <div className="sponsor-wrapper">
-        {
-          sponsors.map((sponsor,index) => (
-            <SponsorCard link={sponsor.link} img={sponsor.img} key={index}/>
-          ))
-          
-        }
-        </div>
-      </section>
-    </>
-  );
+        take: -3,
+    });
+    return (
+        <>
+            <section className="home-a atf-section">
+                <div className="home-a-content">
+                    <h1 className="home-title">
+                        For The Love <br /> of The Game.
+                    </h1>
+                    <p className="standard-text">
+                        Auckland University Esports Club (AUEC) is New Zealand's
+                        biggest student e-sports organization, empowering
+                        players to compete and enjoy gaming with their peers.
+                    </p>
+                </div>
+                <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLScOvsq5IQ-yDr0wx3xYN3ZNJkCz1mP4Kw8NZl6XQ_HSc8rswg/viewform?usp=header"
+                    target="_blank"
+                    className="button-standard button-lime"
+                >
+                    Sign up to AUEC.
+                </a>
+            </section>
+            <section className="home-b">
+                <div className="home-b-top">
+                    <h2 className="section-title">Featured Events</h2>
+                    <StandardButton
+                        title="Explore Events."
+                        color="grey"
+                        link="/events"
+                    />
+                </div>
+                <div className="events-wrapper">
+                    {events.map((event, index) => (
+                        <EventCard event={event} key={index} />
+                    ))}
+                </div>
+            </section>
+            <div className="divider"></div>
+            <section className="home-c">
+                <div className="home-c-img-wrapper-outer">
+                    <div className="home-c-img-wrapper-left">
+                        <img
+                            className="home-c-img-left"
+                            src="DSC00032.webp"
+                            alt=""
+                        />
+                    </div>
+                    <div className="home-c-img-wrapper-right">
+                        <img
+                            className="home-c-img-right"
+                            src="DSC07430.webp"
+                            alt=""
+                        />
+                        <img
+                            className="home-c-img-right"
+                            src="DSC00276.webp"
+                            alt=""
+                        />
+                    </div>
+                </div>
+                <h2 className="home-title">
+                    Meet the <br /> Esports Club.
+                </h2>
+                <p className="standard-text">
+                    From fighting game locals to interclub tournaments, our team
+                    runs a bunch of events for the community and puts a lot of
+                    work into our broadcasts for the folks at home. Whether you
+                    want to grind matches or get involved behind the scenes,
+                    AUEC has a place for you.
+                </p>
+                <StandardButton
+                    title="Learn More."
+                    color="grey"
+                    link="/about"
+                />
+            </section>
+            <div className="divider"></div>
+            <section className="home-b">
+                <div className="home-b-top">
+                    <h2 className="section-title">Community Partners</h2>
+                    <a
+                        href="AUEC 2026 Sponsorship Deck.pdf"
+                        target="_blank"
+                        className="button-standard button-grey"
+                    >
+                        About Sponsorships.
+                    </a>
+                </div>
+                <div className="sponsor-wrapper">
+                    {sponsors.map((sponsor, index) => (
+                        <SponsorCard
+                            link={sponsor.link}
+                            img={sponsor.img}
+                            key={index}
+                        />
+                    ))}
+                </div>
+            </section>
+        </>
+    );
 }
