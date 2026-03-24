154b69e8ec7f5ac2ca96721fc66bf80cd03900ef
TonyDangTran <minh.dangtran@my.bdsc.school.nz>
2026-03-25 00:11:24 +1300

    REFACTOR: changed hamburger menu to only show on smaller resolutions

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,55 +1,80 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { Sling as Hamburger } from 'hamburger-react'
-import StandardButton from '../StandardButton';
-import NavbarPages from './NavbarPages';
-import NavItem from './NavItem';
-import SocialMediaLinkBar from '../SocialMediaLinkBar';
+import { useEffect, useState } from "react";
+import { Sling as Hamburger } from "hamburger-react";
+import StandardButton from "../StandardButton";
+import NavbarPages from "./NavbarPages";
+import NavItem from "./NavItem";
+import SocialMediaLinkBar from "../SocialMediaLinkBar";
 
-
-export default function Navbar(){
-    const [isOpen, setOpen] = useState(false)
+export default function Navbar() {
+    const [isOpen, setOpen] = useState(false);
     useEffect(() => {
         document.body.style.overflow = isOpen ? "hidden" : "auto";
     }, [isOpen]);
-    return(
+    return (
         <>
-            <div className='top-layer'>
-                <header className='navbar'>
+            <div className="top-layer">
+                <header className="navbar">
                     <a href="/">
-                    <img className='navbar-logo' src="auec_logo.svg" alt="" />
+                        <img
+                            className="navbar-logo"
+                            src="auec_logo.svg"
+                            alt=""
+                        />
                     </a>
-                    <Hamburger size={28} toggled={isOpen} toggle={setOpen} />
+                    <div className="navbar-hamburger">
+                        <Hamburger
+                            size={28}
+                            toggled={isOpen}
+                            toggle={setOpen}
+                        />
+                    </div>
                 </header>
                 {isOpen && (
-                    <nav className='navbar-open'>
-                            <ul className='navbar-menu'>
-                                {
-                                    NavbarPages.map((page, index) =>
-                                        <NavItem key={index} onClick={() => setOpen(false)} page={page} index={index + 1}/>
-                                    )
-                                }
-                                <a href="AUEC 2026 Sponsorship Deck.pdf" target="_blank">
-                                    <li className="nav-item" onClick={() => setOpen(false)}>
-                                        <h1 className="nav-item-title">Sponsors</h1>
-                                        <p className="nav-item-index">04</p>
-                                    </li>
-                                </a>
-                                <a href="mailto:uoaesports@gmail.com">
-                                    <li className="nav-item" onClick={() => setOpen(false)}>
-                                        <h1 className="nav-item-title">Contact</h1>
-                                        <p className="nav-item-index">05</p>
-                                    </li>
-                                </a>
-
-                            </ul>
-                            <div className='navbar-open-bottom'>
-                                <a href="https://docs.google.com/forms/d/e/1FAIpQLScOvsq5IQ-yDr0wx3xYN3ZNJkCz1mP4Kw8NZl6XQ_HSc8rswg/viewform?usp=header"  target="_blank" className="button-standard button-lime" onClick={() => setOpen(false)}>
-                                    Sign up to AUEC.
-                                </a>
-                                <SocialMediaLinkBar/>
-                            </div>
+                    <nav className="navbar-open">
+                        <ul className="navbar-menu">
+                            {NavbarPages.map((page, index) => (
+                                <NavItem
+                                    key={index}
+                                    onClick={() => setOpen(false)}
+                                    page={page}
+                                    index={index + 1}
+                                />
+                            ))}
+                            <a
+                                href="AUEC 2026 Sponsorship Deck.pdf"
+                                target="_blank"
+                            >
+                                <li
+                                    className="nav-item"
+                                    onClick={() => setOpen(false)}
+                                >
+                                    <h1 className="nav-item-title">Sponsors</h1>
+                                    <p className="nav-item-index">04</p>
+                                </li>
+                            </a>
+                            <a href="mailto:uoaesports@gmail.com">
+                                <li
+                                    className="nav-item"
+                                    onClick={() => setOpen(false)}
+                                >
+                                    <h1 className="nav-item-title">Contact</h1>
+                                    <p className="nav-item-index">05</p>
+                                </li>
+                            </a>
+                        </ul>
+                        <div className="navbar-open-bottom">
+                            <a
+                                href="https://docs.google.com/forms/d/e/1FAIpQLScOvsq5IQ-yDr0wx3xYN3ZNJkCz1mP4Kw8NZl6XQ_HSc8rswg/viewform?usp=header"
+                                target="_blank"
+                                className="button-standard button-lime"
+                                onClick={() => setOpen(false)}
+                            >
+                                Sign up to AUEC.
+                            </a>
+                            <SocialMediaLinkBar />
+                        </div>
                     </nav>
                 )}
             </div>
