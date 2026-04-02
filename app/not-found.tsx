d01e71afb24f16cce835f26d6f6ee67712908c81
snoommyy <seanleelester07@gmail.com>
2026-04-02 23:59:37 +1300

    feat: Created button and small layout of page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,38 @@
+import Link from 'next/link';
+
 export default function NotFound() {
     return (
-        <div>
-            <h2>Page Not Found</h2>
-            <p>Could not find requested page</p>
+        <div style={{
+            display: 'flex', 
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '69.5vh',
+            textAlign: 'center',
+            gap: '15px'
+        }}>
+            <h2 style={{ fontSize: '2rem', color: 'white'}}>
+                404 - Page Not Found
+            </h2>
+
+            <p style={{ color: 'white'}}>
+                Could not find the requested page.
+            </p>
+
+            <Link href="/">
+                <button style={{
+                    marginTop: '20px',
+                    padding: '10px 20px',
+                    backgroundColor: '#666',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '6px',
+                    cursor: 'pointer',
+                    fontSize: '1rem'
+                }}>
+                    Return to Home
+                </button>
+            </Link>
         </div>
     );
 }
