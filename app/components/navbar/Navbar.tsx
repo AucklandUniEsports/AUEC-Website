083c6f9e114238c619b2a7376b23e8f388d1f7cf
TonyDangTran <minh.dangtran@my.bdsc.school.nz>
2026-04-12 17:16:46 +1200

    Added highlight for current page user is on.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { Sling as Hamburger } from "hamburger-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import StandardButton from "../StandardButton";
 import NavbarPages from "./NavbarPages";
 import NavItem from "./NavItem";
@@ -9,30 +11,48 @@ import SocialMediaLinkBar from "../SocialMediaLinkBar";
 
 export default function Navbar() {
     const [isOpen, setOpen] = useState(false);
+    const pathname = usePathname();
+
     useEffect(() => {
         document.body.style.overflow = isOpen ? "hidden" : "auto";
     }, [isOpen]);
+
+    const navLinks = [
+        { href: "/", label: "HOME" },
+        { href: "/events", label: "EVENTS" },
+        { href: "/about", label: "ABOUT" },
+    ];
+
     return (
         <>
             <div className="top-layer">
                 <header className="navbar">
-                    <a href="/">
+                    <Link href="/">
                         <img
                             className="navbar-logo"
                             src="auec_logo.svg"
                             alt=""
                         />
-                    </a>
+                    </Link>
+
                     <nav className="navbar-items">
-                        <a href="/" className="navbar-link">
-                            HOME
-                        </a>
-                        <a href="/events" className="navbar-link">
-                            EVENTS
-                        </a>
-                        <a href="/about " className="navbar-link">
-                            ABOUT
-                        </a>
+                        {navLinks.map((link) => (
+                            <Link
+                                key={link.href}
+                                href={link.href}
+                                className={`navbar-link ${
+                                    link.href === "/"
+                                        ? pathname === "/"
+                                            ? "active"
+                                            : ""
+                                        : pathname.startsWith(link.href)
+                                          ? "active"
+                                          : ""
+                                }`}
+                            >
+                                {link.label}
+                            </Link>
+                        ))}
                         <a
                             href="AUEC 2026 Sponsorship Deck.pdf"
                             className="navbar-link"
@@ -40,6 +60,7 @@ export default function Navbar() {
                         >
                             SPONSORS
                         </a>
+
                         <a
                             href="mailto:uoaesports@gmail.com"
                             className="navbar-link"
@@ -47,6 +68,7 @@ export default function Navbar() {
                             CONTACT
                         </a>
                     </nav>
+
                     <div className="navbar-hamburger">
                         <Hamburger
                             size={28}
@@ -55,6 +77,7 @@ export default function Navbar() {
                         />
                     </div>
                 </header>
+
                 {isOpen && (
                     <nav className="navbar-open">
                         <ul className="navbar-menu">
@@ -88,6 +111,7 @@ export default function Navbar() {
                                 </li>
                             </a>
                         </ul>
+
                         <div className="navbar-open-bottom">
                             <a
                                 href="https://docs.google.com/forms/d/e/1FAIpQLScOvsq5IQ-yDr0wx3xYN3ZNJkCz1mP4Kw8NZl6XQ_HSc8rswg/viewform?usp=header"
