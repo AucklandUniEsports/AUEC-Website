1d2485f9e83e58d2fdd9be429d9a15995d1642ab
Stephen Wu <Swu676@aucklanduni.ac.nz>
2026-05-08 20:21:06 +1200

    FIX: correct api/events/featured fetch URL in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeClient from "./_components/home/HomeClient";
 
 export default async function Home() {
     const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/featured`,
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/events/featured`,
         {
             cache: "no-store",
         },
