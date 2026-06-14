d2968d943dd17791ed293273bd99022b4fc0fbeb
BlackPri0r <lawrenceli630@gmail.com>
2026-06-14 23:28:42 +1200

    FEAT: blur

--- app/merch/_components/entry/Entry.tsx
+++ app/merch/_components/entry/Entry.tsx
@@ -19,6 +19,8 @@ export function Entry({ onEnter }: EntryProps) {
                 fill
                 priority
                 className="object-cover object-center"
+                placeholder="blur"
+                blurDataURL="/countdown_blur.webp"
             />
             <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center">
                 <EntryTitle />
