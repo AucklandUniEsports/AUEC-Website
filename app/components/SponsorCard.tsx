b80c4cc7e8c63313ca0487b9ecaf8a25ace0b3c2
Stephen Wu <Swu676@aucklanduni.ac.nz>
2026-06-21 22:50:00 +1200

    FIX: resolve sponsor logo URLs from Supabase Storage

--- app/components/SponsorCard.tsx
+++ app/components/SponsorCard.tsx
@@ -1,12 +1,16 @@
 type SponsorCardProps = {
-    link: string,
-    img: string
-}
+    link: string;
+    img: string;
+};
 
-export default function SponsorCard({link, img}: SponsorCardProps){
-    return(
+export default function SponsorCard({ link, img }: SponsorCardProps) {
+    return (
         <a className="sponsor-card" href={link} target="_blank">
-            <img className="sponsor-icon" src={img} alt="Company Logo" />
+            <img
+                className="sponsor-icon"
+                src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/event_thumbnails/${img}`}
+                alt="Company Logo"
+            />
         </a>
-    )
+    );
 }
