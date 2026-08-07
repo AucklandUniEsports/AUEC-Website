07db8e2125091fda0b8b5311a4fa9a03b5f95c99
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2026-08-08 00:05:16 +1200

    fix: build issue

---
+++ app/(frontend)/team/page.tsx
@@ -0,0 +1,10 @@
+export default function Team() {
+    return (
+        <div className="mt-36 ml-16">
+            <h1 className="text-[100px] text-white font-['Syne'] font-medium leading-0.8 tracking-[1.2px] uppercase">
+                Team
+            </h1>
+            <p className="text-white text-lg mt-8">Coming soon.</p>
+        </div>
+    );
+}
