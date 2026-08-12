a9d8ae61c0cd645e8aac5878766b2e525e09baee
Brody <=>
2026-08-13 10:26:45 +1200

    FEAT: added sponsors wrapper to allow for dynamic adding and removing of sponosrs

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -1,3 +1,24 @@
+import SponsorCard from "../components/SponsorCard";
+
+const sponsors = [
+    {
+        link: "https://www.auckland.ac.nz/en/on-campus/facilities-and-services/sport-and-recreation/sport/EsportsArena.html",
+        img: "esports-arena.webp",
+    },
+    {
+        link: "https://gameon.co.nz/",
+        img: "game-on.webp",
+    },
+    {
+        link: "https://www.facebook.com/StandingFierce/",
+        img: "standing-fierce.webp",
+    },
+    {
+        link: "https://zowie.benq.com/en-au/index.html",
+        img: "zowie.webp",
+    },
+];
+
 export default function Sponsors() {
     return (
         <div>
@@ -8,8 +29,8 @@ export default function Sponsors() {
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black to-[#4700ED] opacity-75" />
                 <div className="relative mt-24 z-10 flex flex-col items-center">
-                    <h1 className="text-[100px] text-white font-['Syne'] font-medium leading-0.8 tracking-[1.2px] uppercase text-cente font-bold">
-                        Sponsors
+                    <h1 className="text-[32px] sm:text-[48px] md:text-[72px] lg:text-[96px] text-white font-['Syne'] font-bold tracking-widest sm:tracking-[0.15em] uppercase text-center mx-6 sm:mx-8 md:mx-12 lg:mx-16">
+                        Sponsorships
                     </h1>
                     <p className="text-[18px] text-white font-['Syne'] font-medium leading-0.8 tracking-[1.2px] text-center max-w-xl">
                         Proud to be supported by blah blah lots of sponsors very
@@ -18,7 +39,7 @@ export default function Sponsors() {
                     </p>
                 </div>
             </div>
-            <div className="grid grid-cols-3 gap-12 mt-16 mx-32">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 md:mt-16 mx-8 sm:mx-12 md:mx-20 lg:mx-32">
                 <img
                     src="/DSC00276.webp"
                     alt="Event Picture 1"
@@ -36,10 +57,11 @@ export default function Sponsors() {
                 />
             </div>
 
-            <h1 className="text-[64px] text-white font-['Syne'] font-medium leading-0.8 tracking-[1.2px] uppercase grid place-items-center mt-16">
+            <h1 className="text-[32px] md:text-[64px] text-white font-['Syne'] font-medium leading-[0.8] tracking-[0.15em] uppercase text-center mt-16 mx-4 md:mx-16">
                 Why work with us?
             </h1>
-            <p className="text-[18px] text-white font-['Syne'] font-medium leading-0.8 tracking-[1.2px] text-center max-w-[36rem] mx-auto">
+
+            <p className="text-[18px] text-white font-['Syne'] font-medium leading-0.8 tracking-[1.2px] text-center max-w-[36rem] mx-auto mt-4 font-stretched-extra-expanded">
                 Proud to be supported by blah blah lots of sponsors very cool
                 give us all your money, showcasing sponsors and trying to get
                 new sponsors from this page lol.
@@ -51,22 +73,25 @@ export default function Sponsors() {
                 className="flex justify-center mt-8"
             >
                 <button
-                    className="text-black bg-[#e2ff00] rounded py-2 px-6 mb-4 disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed grid place-items-center w-full-[36rem]"
+                    className="text-[18px] text-black bg-[#e2ff00] rounded py-4 px-8 mb-4 disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed grid place-items-center w-full-[36rem]"
                     type="submit"
                 >
                     Learn More
                 </button>
             </a>
 
-            <h1 className="text-[64px] text-white font-['Syne'] font-medium leading-0.8 tracking-[1.2px] uppercase grid place-items-center ">
+            <h1 className="text-[32px] md:text-[64px] text-white font-['Syne'] font-medium leading-[0.8] tracking-[0.15em] uppercase text-center mx-4 mt-16 md:mx-16">
                 Our 2026 Community Partners
             </h1>
 
-            <div className="grid grid-cols-4 gap-4 my-8 mx-16">
-                <img src="/esports-arena.webp" className="partners-bg" />
-                <img src="/game-on.webp" className="partners-bg" />
-                <img src="/standing-fierce.webp" className="partners-bg" />
-                <img src="/zowie.webp" className="partners-bg" />
+            <div className="sponsor-wrapper my-8 mx-4 md:mx-16">
+                {sponsors.map((sponsor, index) => (
+                    <SponsorCard
+                        key={index}
+                        link={sponsor.link}
+                        img={sponsor.img}
+                    />
+                ))}
             </div>
         </div>
     );
