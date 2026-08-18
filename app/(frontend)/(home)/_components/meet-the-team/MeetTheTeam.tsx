cee11e2fcca15324503c2e341b7e6d7f1fc08e5e
TonyDangTran <minh.dangtran@my.bdsc.school.nz>
2026-08-18 16:23:02 +1200

    FEAT: created div to center elements and added inline tailwindcss

--- app/(frontend)/(home)/_components/meet-the-team/MeetTheTeam.tsx
+++ app/(frontend)/(home)/_components/meet-the-team/MeetTheTeam.tsx
@@ -1,21 +1,29 @@
-import StandardButton from "../../../_components/StandardButton";
+import Link from "next/link";
 
 export default function TeamSection() {
     return (
-        <section className="home-d">
-            <img className="home-d-bg" src="DSC00858.webp" alt="" />
-            <div className="home-d-content">
-                <h2 className="home-title">Meet the Team.</h2>
-                <p className="standard-text">
+        <section className="relative flex h-[100svh] items-center justify-center text-center">
+            <img
+                className="absolute h-full object-cover"
+                src="DSC00858.webp"
+                alt=""
+            />
+            <div className="relative flex max-w-[720px] flex-col items-center gap-5">
+                <h2 className="font-['Syne'] text-[72px] font-medium leading-[80%] text-white ">
+                    MEET THE TEAM.
+                </h2>
+
+                <p className="max-w-[560px] font-['Syne'] text-[24px] leading-none tracking-[-0.7px] text-white">
                     The execs, game reps, broadcast crew and volunteers who keep
                     AUEC running. Built by students, for students. Meet the
                     people behind the matches.
                 </p>
-                <StandardButton
-                    title="Learn More."
-                    color="lime"
-                    link="/about"
-                />
+                <Link
+                    href="/about"
+                    className="rounded-[5px] bg-[#e2ff00] px-8 py-2 font-['Switzer'] text-xl font-medium text-black"
+                >
+                    Learn More.
+                </Link>
             </div>
         </section>
     );
