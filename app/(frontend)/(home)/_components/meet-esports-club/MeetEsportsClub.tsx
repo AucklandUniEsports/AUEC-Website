35b5d6298028ba6a42ae0bdcc8f163d7e67df5a9
vi0letchen <violethongjinchen@gmail.com>
2026-08-18 23:58:47 +1200

    style: removing global css styles + spacing adjustment

--- app/(frontend)/(home)/_components/meet-esports-club/MeetEsportsClub.tsx
+++ app/(frontend)/(home)/_components/meet-esports-club/MeetEsportsClub.tsx
@@ -7,7 +7,7 @@ export default function AboutSection() {
                 <div className="home-c-img-wrapper-left">
                     <img
                         className="home-c-img-left"
-                        src="DSC00032.webp"
+                        src="DSC00276.webp"
                         alt=""
                     />
                 </div>
@@ -19,15 +19,15 @@ export default function AboutSection() {
                     />
                     <img
                         className="home-c-img-right"
-                        src="DSC00276.webp"
+                        src="DSC00032.webp"
                         alt=""
                     />
                 </div>
             </div>
-            <h2 className="home-title -mt-22">
+            <h2 className="text-white font-['Syne'] text-[100px] scale-y-60 font-medium not-italic leading-[0.8] tracking-[1.2px] uppercase -mt-24 text-5xl">
                 MEET THE <br /> ESPORTS CLUB.
             </h2>
-            <p className="standard-text opacity-80">
+            <p className="text-white font-['Syne'] text-[22px] font-normal not-italic leading-[1.0] tracking-[0.2px] opacity-80 max-w-[60%] -mt-2 mb-3">
                 From fighting game locals to interclub tournaments, our team
                 runs a bunch of events for the community and puts a lot of work
                 into our broadcasts for the folks at home. Whether you want to
