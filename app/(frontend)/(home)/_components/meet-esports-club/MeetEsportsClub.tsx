ae3d55106058290082b00a4f02ff5886afb710ba
vi0letchen <violethongjinchen@gmail.com>
2026-08-21 00:11:13 +1200

    style: mobile restyling

--- app/(frontend)/(home)/_components/meet-esports-club/MeetEsportsClub.tsx
+++ app/(frontend)/(home)/_components/meet-esports-club/MeetEsportsClub.tsx
@@ -2,16 +2,16 @@ import StandardButton from "../../../_components/StandardButton";
 
 export default function AboutSection() {
     return (
-        <section className="flex flex-col pt-8 pb-12 gap-4">
-            <div className="flex gap-4 object-cover h-[50vh] w-full">
-                <div className="w-[55%]">
+        <section className="flex flex-col px-7 md:px-0 pt-8 pb-12 gap-4">
+            <div className="flex gap-4 object-cover h-[45vh] md:h-[65vh] w-full">
+                <div className="w-full md:w-[55%]">
                     <img
                         className="object-cover h-full w-full"
                         src="DSC00276.webp"
                         alt=""
                     />
                 </div>
-                <div className="flex flex-col gap-4 w-[45%]">
+                <div className="hidden md:flex flex-col gap-4 w-[45%]">
                     <img
                         className="object-cover h-1/2 w-full"
                         src="DSC00032.webp"
@@ -24,11 +24,11 @@ export default function AboutSection() {
                     />
                 </div>
             </div>
-            <div className="pl-16 flex flex-col gap-4 -mt-24">
-                <h2 className="text-white font-['Syne'] text-[100px] scale-y-70 font-medium not-italic leading-[0.8] tracking-[1.2px] uppercase text-5xl">
+            <div className="pl-5 md:pl-16 flex flex-col gap-4 -mt-10 md:-mt-24">
+                <h2 className="text-white font-['Syne'] text-[40px] md:text-[60px] lg:text-[100px] scale-y-75 font-medium not-italic leading-[0.8] tracking-[1.2px] uppercase">
                     MEET THE ESPORTS <br /> CLUB.
                 </h2>
-                <p className="text-white font-['Syne'] text-[22px] font-normal not-italic leading-[1.0] tracking-[0.2px] opacity-80 max-w-[60%] mb-5">
+                <p className="text-white font-['Syne'] text-[15px] md:text-[22px] font-normal not-italic leading-[1.0] tracking-[0.2px] opacity-80 md:max-w-[60%] mb-5">
                     From fighting game locals to interclub tournaments, our team
                     runs a bunch of events for the community and puts a lot of
                     work into our broadcasts for the folks at home. Whether you
