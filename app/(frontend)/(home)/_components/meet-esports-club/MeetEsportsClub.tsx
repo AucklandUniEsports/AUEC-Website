76ab333950d5ff11ffd3b238e462b0906adcde83
vi0letchen <violethongjinchen@gmail.com>
2026-08-25 17:43:30 +1200

    styling: mobile responsiveness fix

--- app/(frontend)/(home)/_components/meet-esports-club/MeetEsportsClub.tsx
+++ app/(frontend)/(home)/_components/meet-esports-club/MeetEsportsClub.tsx
@@ -3,7 +3,7 @@ import StandardButton from "../../../_components/StandardButton";
 export default function AboutSection() {
     return (
         <section className="flex flex-col px-7 md:px-0 pt-8 pb-12 gap-4">
-            <div className="flex gap-4 object-cover h-[45vh] md:h-[65vh] w-full">
+            <div className="flex gap-4 object-cover h-[30vh] md:h-[45vh] lg:h-[65vh] w-full">
                 <div className="w-full md:w-[55%]">
                     <img
                         className="object-cover h-full w-full"
@@ -24,8 +24,8 @@ export default function AboutSection() {
                     />
                 </div>
             </div>
-            <div className="pl-5 md:pl-16 flex flex-col gap-4 -mt-10 md:-mt-24">
-                <h2 className="text-white font-['Syne'] text-[40px] md:text-[60px] lg:text-[100px] scale-y-75 font-medium not-italic leading-[0.8] tracking-[1.2px] uppercase">
+            <div className="pl-3 md:pl-5 lg:pl-16 flex flex-col gap-4 -mt-10 md:-mt-24">
+                <h2 className="text-white font-['Syne'] text-[30px] md:text-[60px] lg:text-[100px] scale-y-75 font-normal md:font-medium not-italic leading-[0.9] md:leading-[0.8] tracking-[1.2px] uppercase">
                     MEET THE ESPORTS <br /> CLUB.
                 </h2>
                 <p className="text-white font-['Syne'] text-[15px] md:text-[22px] font-normal not-italic leading-[1.0] tracking-[0.2px] opacity-80 md:max-w-[60%] mb-5">
